a78b010d2df1d300272c8ce26e05f6f5a2abeb94
vishakha <vishakha.salke21@gmail.com>
2022-07-06 21:31:39 -0700

    first

---
+++ src/Airtel/HomePage.java
@@ -0,0 +1,12 @@
+package Airtel;
+
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.driver.chrome","D:/Java/AirtelProject/chromeDriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
